a327596b175b32bee53eac3588248435dd1863ae
Hariharan J <135329695+haritech005@users.noreply.github.com>
2025-09-25 14:34:05 +0530

    Code update on Binary search

--- searching/binarySearch.java
+++ searching/binarySearch.java
@@ -15,3 +15,37 @@
 // -> If the key is smaller than the middle element, then the left side is used for next search.
 // -> If the key is larger than the middle element, then the right side is used for next search.
 // This process is continued until the key is found or the total search space is exhausted.
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+    //  Code for binary search
+    int[] arr = {1,2,3,4,5,6};
+    int target = 6;
+    int low = 0;
+    int high = arr.length-1;
+    boolean isFound = false;
+    
+    while (low<=high){
+        int mid = (low+high)/2;
+        if(target == arr[mid]){
+            System.out.println(mid);
+            isFound = true;
+            break;
+        }
+        
+        if(target<arr[mid]){
+            high = mid-1;
+        }
+        else{
+            low = mid +1;
+        }
+    }
+    if(!isFound){
+        System.out.println(-1);
+    }
+    }
+}
