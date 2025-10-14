a59e3ebae7433eda9b1dc4f0fed45241ad3252e7
Hariharan J <135329695+haritech005@users.noreply.github.com>
2025-10-14 07:32:36 +0530

    Update lowerBoundBinary.java

--- searching/lowerBoundBinary.java
+++ searching/lowerBoundBinary.java
@@ -1,23 +1,25 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 class Main {
     public static void main(String[] args) {
-       int[] arr = {1,2,3,4,5,6};
-       int x = 69;
-       int low = 0;
-       int high = arr.length-1;
-       int ans = -1;
-       boolean isFound = false;
-       
-       while(low<=high){
-           int mid = (low+high)/2;
-           if(arr[mid]>=x){
-               ans = mid;
-               high = mid-1;
-               isFound = true;
-           }
-           else{
-               low = mid+1;
-           }
-       }
-           System.out.println(ans);
+        
+        int[] arr = {1,2,2,3};
+        int low = 0;
+        int high = arr.length-1;
+        int index = -1;
+        int x= 31;
+        
+        while(low<high){
+            int mid = low+high/2;
+            if(arr[mid]>=x){
+                index = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        System.out.println(index);
     }
 }
