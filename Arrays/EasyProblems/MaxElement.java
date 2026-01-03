279a33c95cb23ef5c8c05882db0bf99012270737
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-01-02 20:31:47 -0800

    Update MaxElement.java

--- Arrays/EasyProblems/MaxElement.java
+++ Arrays/EasyProblems/MaxElement.java
@@ -1,1 +1,14 @@
+//Bruteforce solution for Finding maximum element in an array
 
+class Main {
+    public static void main(String[] args) {
+       int [] arr = {3,2,1,5,2,4};
+       int max = arr[0];
+       for(int i=1;i<=arr.length-1;i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+       }
+       System.out.println("The maximum value of the array is "+max);
+    }
+}
