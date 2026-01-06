df8478d7621591ab6421c88863c69279f25fec47
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-01-06 09:09:02 -0800

    Add ArraySorted3.java to check if an array is sorted

---
+++ Arrays/EasyProblems/ArraySorted3.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,5,8,0};
+        boolean isSorted = true;
+
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                isSorted=false;
+                break;
+            }
+        }
+        
+        if(isSorted){
+            System.out.println("The array is sorted");
+        }
+        else{
+            System.out.println("The array is not sorted");
+        }
+    }
+}
