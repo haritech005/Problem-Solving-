2344cae66f27ce94c6cc7b3d1dd3ac3dacdaef57
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-01-24 23:05:59 -0800

    Add XOR method for finding missing number
    Implemented XOR method to find the missing number in an array.

--- Arrays/EasyProblems/FindMissing11.java
+++ Arrays/EasyProblems/FindMissing11.java
@@ -9,3 +9,27 @@ class Solution {
         return (value - sum);
     }
 }
+
+//Using XOR method 
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       int [] arr = {0,1,2,3,4};
+       int n = 5;
+       
+       int xor1 = 0;
+       for(int i=1;i<=n;i++){
+           xor1 = xor1^i;
+       }
+       
+       int xor2 = 0;
+       for(int i=0;i<n;i++){
+           xor2 = xor2^arr[i];
+       }
+       
+       System.out.println((xor1^xor2));
+    }
+}
