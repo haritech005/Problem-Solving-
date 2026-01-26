99fb284a2862c9bbd954c364b859e35113d12f49
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-01-25 20:45:00 -0800

    Add optimal subarray sum solution with two pointers
    Implement optimal solution for subarray sum using two pointers.

--- Arrays/EasyProblems/MaxSubArray13.java
+++ Arrays/EasyProblems/MaxSubArray13.java
@@ -54,5 +54,32 @@ class Main {
 }
 
 
+//Optmimal solution with two pointer
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = nums[0];
+        int right = 0;
+        int left = 0;
+        int n = nums.length;
+        int maxLen = 0;
+
+        while (right < n) {
+            while (left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            right++;
+            if (right < n) {
+                sum += nums[right];
+            }
+        }
+        return maxLen;
+    }
+}
+
 
 
