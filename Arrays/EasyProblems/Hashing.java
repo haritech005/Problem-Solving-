04a1221abf8bd4aa2e8e3f30e1ddf5a2bf96e68c
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-02-09 17:49:22 -0800

    Implement element counting using HashMap
    Added a HashMap to count occurrences of elements in an array.

--- Arrays/EasyProblems/Hashing.java
+++ Arrays/EasyProblems/Hashing.java
@@ -9,3 +9,19 @@ class Main {
        System.out.println(hash[2]);
     }
 }
+
+import java.util.HashMap;
+
+class Main {
+    public static void main(String[] args) {
+       int[] arr = {1,1,2,2,1,3,3,4,1};
+       HashMap<Integer,Integer> hashmap = new HashMap<>();
+       
+       for(int x : arr){
+           hashmap.put(x, hashmap.getOrDefault(x,0) +1);
+       }
+       
+       System.out.println(hashmap.getOrDefault(1,0));
+       
+    }
+}
