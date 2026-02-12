aa13fea265874ece1045f99203f1f9c088a65685
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-02-12 08:34:32 -0800

    Add optimized longest subarray sum algorithm
    Optimized the algorithm to find the longest subarray with a sum equal to k for positive integers.

--- Arrays/EasyProblems/LongestSubarrayPositive.java
+++ Arrays/EasyProblems/LongestSubarrayPositive.java
@@ -58,3 +58,32 @@ class Main {
        System.out.println(maxLen);
     }
 }
+
+//optimized only for positives
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 0, 0, 0, 3};
+        int k = 3;
+        int left = 0;
+        int sum = 0;
+        int maxLen = 0;
+
+        for(int right =0; right <arr.length;right ++){
+            sum+=arr[right];
+
+            while(sum>k){
+                sum-=arr[left];
+                left++;
+            }
+
+            if(sum == k){
+                maxLen = Math.max(maxLen,right-left+1);
+            }
+        }
+
+        System.out.println(maxLen);
+
+    }
+}
