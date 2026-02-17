638880892de8882ce1bdfd5c969c4e6b1dcd78ee
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-02-17 08:11:19 -0800

    Implement Insertion Sort in allSortingTechnique.java
    Added implementation of Insertion Sort to the sorting techniques.

--- sorting/allSortingTechnique.java
+++ sorting/allSortingTechnique.java
@@ -18,3 +18,26 @@ class Main {
        System.out.println(Arrays.toString(arr));
     }
 }
+
+//Selection Sort
+
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,5,1,4,7};
+
+        for(int i=1;i<arr.length;i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            arr[j+1] = key;
+        }
+
+    System.out.println(Arrays.toString(arr));
+    }
+}
