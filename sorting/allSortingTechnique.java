d921bac25de0688765982948e8c7b7b51a8951cb
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-02-17 08:15:27 -0800

    Implement Bubble Sort in allSortingTechnique.java
    Added Bubble Sort implementation to the sorting techniques.

--- sorting/allSortingTechnique.java
+++ sorting/allSortingTechnique.java
@@ -19,8 +19,8 @@ class Main {
     }
 }
 
-//Selection Sort
 
+//Insertion Sort
 import java.util.Arrays;
 class Main {
     public static void main(String[] args) {
@@ -39,5 +39,26 @@ class Main {
         }
 
     System.out.println(Arrays.toString(arr));
+    }
+}
+
+//Bubble Sort
+
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+         int[] arr = {3,5,1,4,7};
+         for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+         }
+
+         System.out.println(Arrays.toString(arr));
     }
 }
