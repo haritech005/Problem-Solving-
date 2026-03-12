bb3fb8a69e66b29b8796fe131a9df161d0a4db00
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-03-12 10:58:46 -0700

    Add optimal approach to find leaders in an array
    Implemented an optimal approach to find leaders in an array using a reverse traversal and maintaining a maximum value.

--- Arrays/MediumProblems/Leaders.java
+++ Arrays/MediumProblems/Leaders.java
@@ -49,3 +49,27 @@ class Main {
     }
     
 }
+
+
+//Optimal Approach
+
+import java.util.ArrayList;
+class Main {
+    public static void main(String[] args) {
+       int[] arr = {10,22,12,3,0,6};
+       ArrayList<Integer> result = new ArrayList<>();
+      
+      int max = Integer.MIN_VALUE;
+      for(int i=arr.length-1;i>=0;i--){
+          if(arr[i]>max){
+              result.add(arr[i]);
+              max = arr[i];
+          }
+          
+      }
+      
+      System.out.println(result);
+      
+    }
+    
+}
