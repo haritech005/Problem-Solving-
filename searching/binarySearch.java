3d2dba31137f3dffa0379780bff655a5e96f34c2
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-03-24 23:41:33 +0530

    Add binary search implementation in Solution class
    Implemented binary search algorithm in the Solution class.

--- searching/binarySearch.java
+++ searching/binarySearch.java
@@ -80,3 +80,23 @@ int [] arr = {1,2,3,4,5,6};
         return binary(arr,low,high,target);
     }
 }
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
