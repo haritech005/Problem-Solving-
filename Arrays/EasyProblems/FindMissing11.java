12b22f9c8f5b6000734fb7078d6f6c559d3dc1f7
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-05-16 23:11:54 +0530

    Fix loop syntax in missingNumber method

--- Arrays/EasyProblems/FindMissing11.java
+++ Arrays/EasyProblems/FindMissing11.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int value = n*(n+1)/2;
         int sum = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){ 
             sum = sum+nums[i];
         }
         return (value - sum);
