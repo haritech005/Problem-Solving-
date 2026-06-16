91c377ddc1e311db514c0268d7e42ab7245e40d2
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-06-16 20:58:04 +0530

    Implement longestOnes function to find max length

---
+++ Arrays/MediumProblems/LongestOnes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        int n = nums.length;
+        int zeros = 0;
+
+        while(right <n){
+            if(nums[right]==0){
+                zeros++;
+            }
+
+            while(zeros>k){
+                if(nums[left] == 0){
+                    zeros--;
+                }
+                left++;
+            }
+            maxLen = Math.max(maxLen, right-left+1);
+            right++;
+        }
+        return maxLen;
+    }
+}
