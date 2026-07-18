378ba43628d7a96dc487ddda509685fc761ea26e
Hariharan J <135329695+haritech005@users.noreply.github.com>
2026-07-18 11:03:37 +0530

    Implement recursive linked list reversal
    Added recursive method to reverse a linked list.

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -27,6 +27,8 @@ static Node ReverseBrute(Node head){
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//  Iterative mode
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode temp = head;
@@ -41,4 +43,31 @@ class Solution {
 
         return prev;
     }
+}
+
+
+// Recrusive Mode
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        if(head ==null || head.next ==null) return head;
+
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+
+        return newHead;
+}
 }
